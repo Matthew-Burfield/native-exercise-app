3ebdec79105e3be75e24158d27a729e420e6de6c
Matthew-Burfield <m1.burfield@gmail.com>
2017-06-09 10:58:09 +1000

    Added reqSets and reqReps to initial exercise state

--- src/initialState.js
+++ src/initialState.js
@@ -1,5 +1,5 @@
 export const exercises = [
-  { name: 'Handstand', short: 'HS', pTime: 1, aTime: 2, rTime: 3 },
-  { name: 'Ring Hold', short: 'RH', pTime: 10, aTime: 30, rTime: 60 },
-  { name: 'Pullups', short: 'PU', reps: 5, sets: 5 },
+  { name: 'Handstand', short: 'HS', pTime: 1, aTime: 2, rTime: 3, reqSets: 5 },
+  { name: 'Ring Hold', short: 'RH', pTime: 10, aTime: 30, rTime: 60, reqSets: 5 },
+  { name: 'Pullups', short: 'PU', reqReps: 5, reqSets: 5 },
 ]
