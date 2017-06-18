1522ac2981c557eb9f321e5708bd42f3a48c2772
Matthew-Burfield <m1.burfield@gmail.com>
2017-06-19 08:06:17 +1000

    added more excersises to initial state

--- src/initialState.js
+++ src/initialState.js
@@ -1,5 +1,10 @@
 export const exercises = [
-  { name: 'Handstand', short: 'HS', pTime: 1, aTime: 2, rTime: 3, completedSets: 0, reqSets: 5 },
+  { name: 'Handstand', short: 'HS', pTime: 5, aTime: 10, rTime: 10, completedSets: 0, reqSets: 5 },
   { name: 'Ring Hold', short: 'RH', pTime: 10, aTime: 30, rTime: 60, completedSets: 0, reqSets: 5 },
-  { name: 'Pullups', short: 'PU', reqReps: 5, completedSets: 1, reqSets: 5 },
+  { name: 'Pullups', short: 'PU', reqReps: 5, completedSets: 0, reqSets: 5 },
+  { name: 'Pushups', short: 'PU', reqReps: 5, completedSets: 0, reqSets: 5 },
+  { name: 'Rows', short: 'RW', reqReps: 5, completedSets: 0, reqSets: 5 },
+  { name: 'Dips', short: 'DP', reqReps: 5, completedSets: 0, reqSets: 5 },
+  { name: 'Squats', short: 'SQ', reqReps: 5, completedSets: 0, reqSets: 5 },
+  { name: 'Deadlifts', short: 'DL', reqReps: 5, completedSets: 0, reqSets: 5 },
 ]
