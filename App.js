40a4c8e92bf4a80f846c727c95787484feaf1540
Matthew-Burfield <m1.burfield@gmail.com>
2017-06-19 13:58:00 +1000

    added sound files and react-native-sound dependency

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './src/reducers';
 import { Welcome, Workout } from './src/containers'
+import Sound from 'react-native-sound'
 
 const store = createStore(rootReducer)
 
@@ -10,12 +11,19 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      started: false
+      started: false,
+      beep: new Sound('countdownbeep.wav', Sound.MAIN_BUNDLE),
+      lastBeep: new Sound('countdownfinalbeep.wav', Sound.MAIN_BUNDLE),
     }
   }
 
   startWorkout = () => {
     this.setState({ started: true })
+    this.state.beep.play((success) => {
+      if (success) {
+        this.state.lastBeep.play()
+      }
+    })
   }
 
   renderRoot(ComponentToRender) {
